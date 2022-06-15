f41e4824471648feae36d811f079f54c2baaba73
Mohamed <51891244+mkaouane@users.noreply.github.com>
2022-06-15 16:42:06 +0200

    fetch animes and pagination

---
+++ src/components/CardAnime.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card class="mx-auto" max-width="344">
+    <v-img
+      :src="image"
+      height="200px"
+    ></v-img>
+
+    <v-card-title>{{title}} </v-card-title>
+
+    <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn color="orange lighten-2" target="_blank" :href="link" text> Explore </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon @click="show = !show">
+        <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          {{synopsis}}
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+<script>
+export default {
+   props : ['title', 'image', 'synopsis', 'link'],
+   data() {
+      return {
+         show: false,
+      }
+   }
+}
+</script>
+
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,115 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-switch v-model="$vuetify.theme.dark" inset persistent-hint></v-switch>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col
+            v-for="anime in animes"
+            :key="anime.title"
+            cols="12"
+            sm="12"
+            md="6"
+            lg="4"
+          >
+            <CardAnime
+              :title="anime.title"
+              :image="anime.image"
+              :synopsis="anime.synopsis"
+              :link="anime.link"
+            />
+          </v-col>
+        </v-row>
+        <div class="text-center">
+          <v-pagination v-model="page" @input="nextPage()" :length="animes.length" circle></v-pagination>
+        </div>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import axios from "axios";
+import CardAnime from "./CardAnime.vue";
+import config from "../../config.json";
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+const token = config.RAPIDAPI_KEY;
+
+
+
+export default {
+  components: { CardAnime },
+  data: () => ({
+    drawer: null,
+    animes: [],
+    title: "",
+    show: false,
+    page: 1,
+  }),
+  methods: {
+    toggleCard() {
+      this.show = !this.show;
+    },
+    async getAnimes() {
+      await axios
+        .get("https://anime-db.p.rapidapi.com/anime", {
+          method: "GET",
+          params: {
+            page: this.page,
+            size: "9",
+            //  genres: "Fantasy,Drama",
+            sortBy: "ranking",
+            sortOrder: "asc",
+          },
+          headers: {
+            "X-RapidAPI-Key": `${token}`,
+            "X-RapidAPI-Host": "anime-db.p.rapidapi.com",
+          },
+        }).then(res => this.animes = res.data.data)
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+      nextPage() {
+      this.page + 1;
+      this.getAnimes()
+      console.log(this.page)
+    }
+  },
+  async mounted() {
+    try {
+      await axios
+        .get("https://anime-db.p.rapidapi.com/anime", {
+          method: "GET",
+          params: {
+            page: this.page,
+            size: "9",
+            //  genres: "Fantasy,Drama",
+            sortBy: "ranking",
+            sortOrder: "asc",
+          },
+          headers: {
+            "X-RapidAPI-Key": `${token}`,
+            "X-RapidAPI-Host": "anime-db.p.rapidapi.com",
+          },
+        }).then(res => this.animes = res.data.data)
+        .catch((error) => {
+          console.error(error);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+</script>
